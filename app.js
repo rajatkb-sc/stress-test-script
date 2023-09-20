f46e771a35226477332755a6f015122941a927a3
rajatkb-sc <rajat.bhattacharjee@sharechat.co>
2023-09-21 00:58:57 +0530

    feat: fix the script for better scaling

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const autocannon = require("autocannon")
-const aggregateResult = autocannon.aggregateResult
-
 
 const fs = require('fs');
-const { config } = require("process");
 const readline = require('readline');
 
 const FILE_PATH = 'output.json';
@@ -13,46 +10,66 @@ const FRS_URL = "http://feed-relevance-service.sharechat.internal"
 
 
 // Overrides
-// const RANKER_TEST_VARIANT_TF = "control"
-// const RANKER_TEST_VARIANT_VF = "control"
-// const RANKER_TEST_VARIANT_VSF = "control"
-const RANKER_GLOBAL_FEED_VARIANT = "variant-gf-13"
-const LIMIT = 10
+const RANKER_TEST_VARIANT_TF = "variant-tf-13"
+const RANKER_TEST_VARIANT_VF = "variant-vf-13"
+const RANKER_TEST_VARIANT_VSF = "variant-vs-13"
+const RANKER_GLOBAL_FEED_VARIANT = "variant-gf-15"
+const LIMIT = 20
 
 
 
-const LANGUAGES = [
-    "Bengali",
-    "Hindi",
-    "Gujarati",
-    "Kannada",
-    "Malayalam",
-    "Marathi",
-    "Odia",
-    "Punjabi",
-    "Tamil",
-    "Telugu",
-]
 
-async function* loadJSONL(filePath) {
-
+async function* loadJSONL(filePath, startLine = -1) {
     const fileStream = fs.createReadStream(filePath);
     const rl = readline.createInterface({
         input: fileStream,
         crlfDelay: Infinity
     });
 
+    let lineCount = 0;
+
     for await (const line of rl) {
+        lineCount++;
+        if (lineCount < startLine) {
+            // Skip lines until the desired starting line is reached
+            continue;
+        }
+
         const jsonObject = JSON.parse(line);
-        yield jsonObject
+        yield jsonObject;
     }
 }
 
-async function* batchRequests(batchSize = 100000) {
+
+async function getLineCount(filePath) {
+    return new Promise((resolve, reject) => {
+        exec(`wc -l ${filePath}`, (error, stdout, stderr) => {
+            if (error) {
+                reject(`Error executing wc command: ${error.message}`);
+                return;
+            }
+
+            if (stderr) {
+                reject(`Error from wc command: ${stderr}`);
+                return;
+            }
+
+            // Parse the output to extract the line count
+            const lineCount = parseInt(stdout.trim().split(' ')[0], 10);
+
+            resolve(lineCount);
+        });
+    });
+}
+
+
+
+async function* batchRequests(batchSize = 100000 , randomlineStart = -1) {
+    console.log("using batch size :" , batchSize , " random line :" , randomlineStart)
     let i = 0
     let batchedObjects = []
 
-    for await (const jsonObject of loadJSONL(FILE_PATH)) {
+    for await (const jsonObject of loadJSONL(FILE_PATH , randomlineStart)) {
 
         let endpoint = ""
 
@@ -75,10 +92,6 @@ async function* batchRequests(batchSize = 100000) {
             continue
         }
 
-        const urls = LANGUAGES.map(lang => {
-            return `/${lang}/${endpoint}`
-        })
-
        
         let payload
 
@@ -88,9 +101,11 @@ async function* batchRequests(batchSize = 100000) {
         jsonObject.payload.limit = LIMIT
         jsonObject.payload["syntheticLoad"] = true
 
+        const lang = jsonObject.language
+
         switch (jsonObject.feedType) {
             case "video-feed" : {
-                // jsonObject.payload.experiment.variant = RANKER_TEST_VARIANT_VF
+                jsonObject.payload.experiment.variant = RANKER_TEST_VARIANT_VF
                 jsonObject.payload.experiment.feedRelGlobalFeedExperiment = RANKER_GLOBAL_FEED_VARIANT
                 // jsonObject.payload.experiment.feedRelGlobalRevenueExperiment = "control"
                 payload = {
@@ -104,7 +119,7 @@ async function* batchRequests(batchSize = 100000) {
                 }
             }
             case "trending" : {
-                // jsonObject.payload.experiment.variant = RANKER_TEST_VARIANT_TF
+                jsonObject.payload.experiment.variant = RANKER_TEST_VARIANT_TF
                 jsonObject.payload.experiment.feedRelGlobalFeedExperiment = RANKER_GLOBAL_FEED_VARIANT
                 // jsonObject.payload.experiment.feedRelGlobalRevenueExperiment = "control"
                 payload = {
@@ -119,7 +134,7 @@ async function* batchRequests(batchSize = 100000) {
                 break
             }
             case "video-suggestion": {
-                // jsonObject.payload.variant = RANKER_TEST_VARIANT_VSF
+                jsonObject.payload.variant = RANKER_TEST_VARIANT_VSF
                 jsonObject.payload.feedRelGlobalFeedExperiment = RANKER_GLOBAL_FEED_VARIANT
                 // jsonObject.payload.feedRelGlobalRevenueExperiment = "control"
                 payload = {
@@ -134,16 +149,13 @@ async function* batchRequests(batchSize = 100000) {
             }
         }
 
-        urls.forEach((url) => {
-            batchedObjects.push({
-                ...payload,
-                url,
-            })
-        })
+        payload = {
+            ...payload,
+            url : `/${lang}/${endpoint}`
+        }
 
         i++
-
-
+        
         if (i === batchSize) {
             yield batchedObjects
             i = 0
@@ -162,72 +174,42 @@ async function* batchRequests(batchSize = 100000) {
 (async () => {
 
 
+    const totalLines = getLineCount(FILE_PATH)
+
+    console.log(`reading : ${FILE_PATH} lines total : ${totalLines}`)
 
     const configs = [
         {
-            BATCH_SIZE: 1000000,
-            WORKER: 32,
+            BATCH_SIZE: 100000,
             RATE_OF_REQUEST: 1000,
-            TEST_DURATION: 5 * 60, // warm up
-            CONNECTIONS: 1000,
-            PIPELINING: 1000,
-            STEP_UP : 100,
-        },
-        {
-            BATCH_SIZE: 10000,
-            WORKER: 32,
-            RATE_OF_REQUEST: 2000,
-            TEST_DURATION: 5 * 60, // warm up
-            CONNECTIONS: 1000,
-            PIPELINING: 1000,
-            STEP_UP : 1000,
-        },
-        // smaller batches quicker load 
-        {
-            BATCH_SIZE: 1000,
-            WORKER: 64,
-            RATE_OF_REQUEST: 6000,
-            TEST_DURATION: 60,
+            TEST_DURATION:   60, // warm up
             CONNECTIONS: 1000,
             PIPELINING: 10000,
-            STEP_UP : 2000,
-        },
-        // smaller batches qicker load
-        {
-            BATCH_SIZE: 1000,
-            WORKER: 64,
-            RATE_OF_REQUEST: 8000,
-            TEST_DURATION: 60,
-            CONNECTIONS: 10000,
-            PIPELINING: 1000000,
-            STEP_UP: 3000,
-        },
-        {
-            BATCH_SIZE: 1000,
-            WORKER: 64,
-            RATE_OF_REQUEST: 8000,
-            TEST_DURATION: 60,
-            CONNECTIONS: 10000,
-            PIPELINING: 1000000,
-            STEP_UP: 3000,
-        },
-        {
-            BATCH_SIZE: 1000,
-            WORKER: 64,
-            RATE_OF_REQUEST: 8000,
-            TEST_DURATION: 60,
-            CONNECTIONS: 10000,
-            PIPELINING: 1000000,
-            STEP_UP: 3000,
+            STEP_UP : 50,
+            MAX_RATE_OF_REQUEST: 2000
         },
     ];
-
+   
 
     for (const config of configs) {
+
+        
+        const batchSize = config.BATCH_SIZE
+        
+        const duration = config.TEST_DURATION
+
         const stepUp = config.STEP_UP
+
+        const totalDuration = Math.floor(totalLines / batchSize) * duration
+
+        console.log(`Stress test started : estimated duration ${totalDuration}`)
+
         let i = 0;
-        for await(const obj of batchRequests(config.BATCH_SIZE)) {
-            console.log(`read ${obj.length} records : stressing ${FRS_URL}`)
+
+        const randomStartLine = Math.floor(Math.random() * totalLines) + 1;
+
+        for await(const obj of batchRequests(config.BATCH_SIZE , randomStartLine)) {
+            console.log(`read ${obj.length} records : stressing ${FRS_URL}` , config)
     
             const instance = autocannon({
                 url: FRS_URL,
@@ -237,7 +219,7 @@ async function* batchRequests(batchSize = 100000) {
                 connections : config.CONNECTIONS , 
                 overallRate: config.RATE_OF_REQUEST + i * stepUp, 
                 duration : config.TEST_DURATION , 
-                workers : config.WORKER,
+                workers : 1,
                 requests : obj.map((v => {
                     return {
                         body: JSON.stringify(v.payload),
